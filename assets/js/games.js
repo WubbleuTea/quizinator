16a42b243135514b25f9c37d252649f352d9b077
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-06-14 17:13:00 -0500

    form populates and questions cycle through

--- assets/js/games.js
+++ assets/js/games.js
@@ -1,60 +1,130 @@
-var question = document.getElementById("question")
-
+var questionTitle = document.getElementById("question");
+var answerTitle = document.getElementById("answer-choices");
+var accuracyTitle = document.getElementById("accuracy");
+var getInitials = document.getElementById("initials");
+var savedScore = document.getElementById("saved-score")
+var questionNumber = 0;
 var finalScore = 0;
-
-var time = 6000;
-
+var currentQuestion = {};
+var startCountdown;
 //timer
+
 var timer = 10;
 var countdown = function() {
     var timerEl = document.getElementById('timer');
     timerEl.textContent = timer + " seconds remaining";
-    console.log(timer);
-   
-    if(timer === 0) {
-        alert("Game is over");
-        finalScore = 0;
+       
+    if(timer <= 0) {
+        timer = 0;
         clearInterval(startCountdown);
-        //then this becomes the final score
+        timerEl.textContent = timer + " seconds remaining";
+        questionTitle.remove();
+        answerTitle.remove();
+        accuracyTitle.remove();
+        getInitials.style.visibility = "visible";
+        savedScore.appendChild(timer);
+        console.log("your final score is " + finalScore);
     };
     timer--;
 };
 
-var startCountdown = setInterval(countdown, 1000);
 
 // array of questions
-    //with answers and possibility of correct answer
+    
 var questionsTotal = [
     {
         question: "Here is my first question",
-        answer1: "answer #1-1",
-        answer2: "answer #1-2",
-        answer3: "answer #1-3",
-        answer4: "answer #1-4",
+        choices:  ["1-1", "1-2", "1-3", "1-4"],
+        trueStatement: "1-4"
     },
     {
         question: "Here is my second question",
-        answer1: "answer #2-1",
-        answer2: "answer #2-2",
-        answer3: "answer #2-3",
-        answer4: "answer #2-4",
+        choices:  ["2-1", "2-2", "2-3", "2-4"],
+        trueStatement: "2-2"
     },
-    {
-        question: "Here is my third question",
-        answer1: "answer #3-1",
-        answer2: "answer #3-2",
-        answer3: "answer #3-3",
-        answer4: "answer #3-4",
-    },
-    {
-        question: "Here is my fourth question",
-        answer1: "answer #4-1",
-        answer2: "answer #4-2",
-        answer3: "answer #4-3",
-        answer4: "answer #4-4",
-    },
-    
-]
+ 
+];
+
+
 //gameBegin function
+gameBegin = function() {
+    startCountdown = setInterval(countdown, 1000);
+    nextQuestion();
+}
 
 //nextQuestion Function
+nextQuestion = function() {
+    if (questionNumber < questionsTotal.length) {
+        accuracyTitle.textContent = "";
+        var gameQuestion = questionsTotal[questionNumber];
+            questionTitle.textContent = gameQuestion.question; 
+            answerTitle.textContent = "";
+            gameQuestion.choices.forEach(function(choice) {
+                var answerButton = document.createElement("button");
+                answerButton.textContent = choice;
+                answerButton.setAttribute("value", choice);
+                answerButton.setAttribute("class", "answer-container");
+                answerButton.onclick = chosenAnswer;
+                answerTitle.appendChild(answerButton);
+            });
+    } else {   
+        clearInterval(startCountdown);
+        if (timer < 0) {
+            timer = 0;
+        } else {
+            timer++;
+        }
+        //timer reads one second higher then the final score without the timer++
+        finalScore = timer;
+        questionTitle.remove();
+        answerTitle.remove();
+        accuracyTitle.remove();
+        getInitials.style.visibility = "visible";
+        savedScore.appendChild(finalScore);
+        console.log("your final score is " + finalScore);
+    }
+};
+
+function chosenAnswer() {
+    console.log(this.value);
+    //convert their choice to text and then check if choice = trueStatment  if/else
+    if (this.value === questionsTotal[questionNumber].trueStatement) {
+        var correctAnswer = document.createElement("div");
+        correctAnswer.textContent = "Correct!";
+        correctAnswer.setAttribute("class", "yesorno");
+        accuracyTitle.appendChild(correctAnswer);
+        questionNumber++;
+        setTimeout(nextQuestion, 500);
+    } 
+    else {
+        var wrongAnswer = document.createElement("div");
+        wrongAnswer.textContent = "WRONG!";
+        wrongAnswer.setAttribute("class", "yesorno");
+        accuracyTitle.appendChild(wrongAnswer);
+        timer = timer -5;
+        questionNumber++;
+        setTimeout(nextQuestion, 500);
+    };
+};
+
+
+gameBegin();
+
+
+
+
+
+
+// if(questionNumber === questionsTotal.length) {
+    //     alert("Game is over");
+    //     clearInterval(startCountdown);
+    //     finalScore = timer;
+    //     localStorage.setItem("intials", prompt("Please enter your initaials"));
+    //     localStorage.setItem("score", finalScore);
+    // } else if(questionNumber < questionsTotal.length) {
+    //     currentQuestion = questionsTotal;
+    //     questionsTotal.innerText = currentQuestion.question;
+    // }
+
+    // localStorage.setItem("player", prompt("Please enter your initials"));
+    //     localStorage.setItem("score", finalScore);
---
+++ assets/js/highscore.js
@@ -0,0 +1,3 @@
+// get localStorage info
+
+//appendChild to id high score name and highscore-final
