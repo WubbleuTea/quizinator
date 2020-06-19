c92899e0522ba326268bf38645b179d0fbab2d4a
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-06-19 15:48:28 -0500

    fixed localStorage pull request

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,14 +1,22 @@
 var  displayHS = document.getElementById("display-hs");
 var resetScores = document.getElementById("reset-scores");
 
-var storageScores;
+var storageScores ;
 
 function displayScores() {
    
   var storageScores = JSON.parse(localStorage.getItem("highscore"));
-    storageScores.innerHTML = "<h1 class='title'>" + storageScores.name + "</h3><span class='title'>" + storageScores.score + "</span>";
-    displayHS.appendChild(storageScores);
-   //could not get the scores to append they are in the local storge which can be reset by the button just not displaying on page.
+    storageScores.forEach(function(score) {
+      var initials = document.createElement("p");
+      initials.setAttribute("class", "high-score");
+      var viewedScore = document.createElement("p");
+      viewedScore.setAttribute("class", "high-score");
+      initials.textContent = score.name + " - ";
+      viewedScore.textContent = " " + score.score + "!"; 
+      displayHS.appendChild(initials, viewedScore);
+      displayHS.appendChild(viewedScore);
+    });
+   
 
 };
 
